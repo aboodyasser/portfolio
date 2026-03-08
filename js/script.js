34aebbb90efbbbf35613508f507495212fe00f98
aboodyasser <a409122074@gmail.com>
2026-03-08 10:23:12 +0200

    Updated skills section and centered layou2045863t

--- js/script.js
+++ js/script.js
@@ -4,9 +4,9 @@ const nav = document.getElementById("nav-links");
 const navLinks = nav.querySelectorAll("a");
 
 // فتح وغلق القائمة عند الضغط على ☰
-toggle.onclick = () => {
-  nav.classList.toggle("active");
-};
+// toggle.onclick = () => {
+//   nav.classList.toggle("active");
+// };
 
 // إغلاق القائمة تلقائياً عند الضغط على أي رابط (فقط للشاشات الصغيرة)
 navLinks.forEach(link => {
@@ -45,3 +45,25 @@ AOS.init({
   duration: 1000,
   once: true
 });
+
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLink = document.getElementById("nav-links");
+
+// عند الضغط على الزر
+menuToggle.addEventListener("click", () => {
+    navLink.classList.toggle("active"); // فتح/غلق القائمة
+
+    // تحويل الزر بين ☰ و ✕
+    menuToggle.textContent = menuToggle.textContent.trim() === "☰" ? "✕" : "☰";
+});
+
+// إغلاق القائمة عند الضغط على أي رابط
+navLink.querySelectorAll("a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLink.classList.remove("active"); // إخفاء القائمة
+        menuToggle.textContent = "☰";       // إعادة الزر إلى ☰
+    });
+});
+
+ 
